1c72bf07887337c02f457080a0a00e5271e8371f
pedrovill <pedrovigut@hotmail.com>
2024-02-01 00:15:24 -0500

    Please enter the commit message for your changes. Lines starting

--- user_class.py
+++ user_class.py
@@ -6,19 +6,28 @@
 
 class user:
     fav_list = []
+    def __init__(self):
+        self.password = 0
+        self.username = 0
+        self.fav_list = []
+        print("User started")
+
     def username():
         print("Enter your username:")
         username = input()
-    
+        print("Username set")
+
     def password():
         print("Enter password:")
         password = input()
+        print("Password set")
 
     def fav_stocks():
         print("Enter stock to add to favorites list:")
-        user.fav_list.append()
+        user.fav_list.append(input())
+        print("Stock "+ fav_list[-1]+" was added to you list!")
 
 pedro = user
-pedro.password
+pedro.password()
 
 
